30b0275bdcc760c662206dd96f05779bc9e03e68
redkiyoligarx <nbekdev@gmail.com>
2023-06-07 14:03:02 +0500

    first commit

---
+++ 1.js
@@ -0,0 +1,22 @@
+//Number
+12,12.5
+//BigInt
+12n
+//String
+'' ,"" ,``
+//Symbol
+
+//Boolean
+true ,false
+//Null
+let a=null
+a+1 //1
+//Undefined
+let a 
+a+1 
+//Object
+[1,2,33]
+{
+    'age':21
+}
+
